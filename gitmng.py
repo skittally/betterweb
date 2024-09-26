accc4e42561e8f7a25312467f39d0d0e0e58a888
skittally <skitally@proton.me>
2024-09-26 00:01:12 -0400

    .

--- gitmng.py
+++ gitmng.py
@@ -9,6 +9,6 @@ elif userInpt == "push":
 	user2in = input("input your commit name: ")
 	os.system("git add .")
 	os.system(f'git commit -m "{user2in}"')
-	os.system("git push origin main")
+	os.system("git push origin master")
 else:
 	print("INVALID")
